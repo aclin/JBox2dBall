eec340671d52b93ea9e846d63ab2c25a4781530e
aclin <devilsodo@gmail.com>
2011-02-12 00:42:15 -0800

    Orientation adjust. Ball falls down

--- src/com/alin/jbox2dball/Jbox2dBallView.java
+++ src/com/alin/jbox2dball/Jbox2dBallView.java
@@ -2,7 +2,9 @@ package com.alin.jbox2dball;
 
 import org.jbox2d.collision.AABB;
 import org.jbox2d.collision.CircleDef;
+import org.jbox2d.collision.CircleShape;
 import org.jbox2d.collision.PolygonDef;
+import org.jbox2d.collision.PolygonShape;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.Body;
 import org.jbox2d.dynamics.BodyDef;
@@ -25,8 +27,8 @@ public class Jbox2dBallView extends SurfaceView implements SurfaceHolder.Callbac
 	
 	private World world;
 	private AABB worldAABB;
-	private Body ballBody;
-//	private BodyDef bodyDef;
+	private BodyDef groundBodyDef;
+	private Body groundBody, ballBody;
 	private CircleDef ball;
 	
 	private ballLoop loop;
@@ -43,7 +45,7 @@ public class Jbox2dBallView extends SurfaceView implements SurfaceHolder.Callbac
 		worldAABB.lowerBound.set(new Vec2(0.0f, 0.0f));
 		worldAABB.upperBound.set(new Vec2(150.0f, 150.0f));
 		
-		Vec2 gravity = new Vec2(0.0f, -9.8f);
+		Vec2 gravity = new Vec2(0.0f, 9.8f);
 		boolean doSleep = true;
 		world = new World(worldAABB, gravity, doSleep);
 		createGround();
@@ -51,6 +53,7 @@ public class Jbox2dBallView extends SurfaceView implements SurfaceHolder.Callbac
 	}
 	
 	private class ballLoop extends Thread {
+		private Canvas canvas = null;
 		
 		public void run() {
 			while(!isInterrupted()) {
@@ -65,6 +68,9 @@ public class Jbox2dBallView extends SurfaceView implements SurfaceHolder.Callbac
 		
 		private void updateState() {
 			world.step(timeStep, iterations);
+			
+			//Vec2 posB = ballBody.getPosition();
+	        //Log.v("PHYSICS TEST", "Pos: (" + posB.x + ", " + posB.y + ")");
 		}
 		
 		private void updatePhysics() {
@@ -76,7 +82,6 @@ public class Jbox2dBallView extends SurfaceView implements SurfaceHolder.Callbac
 		}
 		
 		private void updateView() {
-			Canvas canvas = null;
 			Paint mpaint = new Paint();
 			try {
 				canvas = getHolder().lockCanvas();
@@ -85,18 +90,42 @@ public class Jbox2dBallView extends SurfaceView implements SurfaceHolder.Callbac
 				synchronized (this) {
 					mpaint.setStyle(Paint.Style.FILL_AND_STROKE);
 					mpaint.setColor(Color.RED);
-					canvas.drawCircle(ballBody.getPosition().x, ballBody.getPosition().y, ball.radius, mpaint);
+					//canvas.drawCircle(ballBody.getPosition().x, ballBody.getPosition().y, ball.radius, mpaint);
+					canvas.drawCircle(ballBody.getPosition().x, ballBody.getPosition().y, ((CircleShape) ballBody.getShapeList()).getRadius(), mpaint);
+					mpaint.setColor(Color.BLACK);
+					drawPolygon(mpaint);
+					//canvas.drawRect(0.0f, 0.0f, 150.0f, 150.0f, mpaint);
+					//canvas.drawRect(10.0f, 145.0f, 150.0f, 150.0f, mpaint);
 				}
 			} finally {
 				getHolder().unlockCanvasAndPost(canvas);
 			}
 		}
+		
+		private void drawPolygon(Paint mpaint) {
+			PolygonShape ps;
+			Vec2 a, b, c, d;
+			
+			ps = (PolygonShape) groundBody.getShapeList();
+			
+			a = ps.getVertices()[0];
+			b = ps.getVertices()[1];
+			c = ps.getVertices()[2];
+			d = ps.getVertices()[3];
+			
+			canvas.drawRect(ps.getVertices()[0].x + groundBodyDef.position.x,
+							ps.getVertices()[0].y + groundBodyDef.position.y,
+							ps.getVertices()[2].x + groundBodyDef.position.x,
+							ps.getVertices()[2].y + groundBodyDef.position.y,
+							mpaint);
+			//canvas.drawRect(10.0f, 20.0f, 90.0f, 90.0f, mpaint);
+		}
 	}
 	
 	private void createGround() {
-		BodyDef groundBodyDef = new BodyDef();
-		groundBodyDef.position.set(25.0f, 5.0f);
-        Body groundBody = world.createBody(groundBodyDef);
+		groundBodyDef = new BodyDef();
+		groundBodyDef.position.set(60.0f, 150.0f - 30.0f);
+        groundBody = world.createBody(groundBodyDef);
         PolygonDef groundShapeDef = new PolygonDef();
         groundShapeDef.setAsBox(50.0f, 10.0f);
         groundBody.createShape(groundShapeDef);
@@ -106,7 +135,7 @@ public class Jbox2dBallView extends SurfaceView implements SurfaceHolder.Callbac
 		
 		// Create dynamic body
 		BodyDef ballBodyDef = new BodyDef();
-		ballBodyDef.position.set(20.0f, 100.0f);
+		ballBodyDef.position.set(20.0f, 150.0f - 100.0f);
 		ballBody = world.createBody(ballBodyDef);
 		
 		// Create shape with properties
